326078725d3d78e45d29642f697b6f4fe7b37141
Chris <tranuytrieuvi@protonmail.com>
2019-12-15 14:35:26 +0700

    Update files

--- src/main/java/com/chris96/Main.java
+++ src/main/java/com/chris96/Main.java
@@ -1,11 +1,36 @@
 package com.chris96;
 
-import com.chris96.Model.Missle;
-import com.chris96.Model.Gun;
+import java.util.Arrays;
+import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
-        //
+        int width, height, missileAmount;
+
+        try {
+            if (args.length > 0 && args.length <= 3)
+            {
+                width = Integer.parseInt(args[0]);
+                height = Integer.parseInt(args[1]);
+                missileAmount = Integer.parseInt(args[2]);
+            } else {
+                Scanner reader = new Scanner(System.in);
+
+                width = getInput(reader, "base's Width");
+                height = getInput(reader, "base's Height");
+                missileAmount = getInput(reader, "missile amount");
+            }
+        } catch (Exception e) {
+            System.out.println("Invalid input");
+            e.printStackTrace();
+            System.exit(1);
+        }
+
+    }
+
+    private static int getInput(Scanner reader, String mess) {
+        System.out.println("Enter " + mess + " : ");
+        return reader.nextInt();
     }
 }
---
+++ src/main/java/com/chris96/Model/Base.java
@@ -0,0 +1,19 @@
+package com.chris96.Model;
+
+public class Base {
+    private final int WIDTH;
+    private final int HEIGHT;
+
+    public int getWIDTH() {
+        return WIDTH;
+    }
+
+    public int getHEIGHT() {
+        return HEIGHT;
+    }
+
+    public Base(final int width, final int height) {
+        this.WIDTH = width;
+        this.HEIGHT = height;
+    }
+}
